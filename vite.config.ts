eda352bcc0470a17d6013213a53b9f3dd342c928
Koutarou Mori <m.koutarou2004@gmail.com>
2025-09-26 17:09:56 +0900

    https config from vite to devfile

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    https: true,
     host: '0.0.0.0',
     proxy: {
       '/amserver': {
