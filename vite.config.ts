764a3c02266e9a726c121c94c567fed9a910a388
Koutarou Mori <m.koutarou2004@gmail.com>
2025-09-26 10:54:07 +0900

    add https and proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,23 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    https: true,
+    proxy: {
+      '/amserver': {
+        target: 'https://mastersso.kanagawa-u.ac.jp',
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost'
+      },
+      '/campusweb': {
+        target: 'https://webstation.kanagawa-u.ac.jp',
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost'
+      },
+      '/__/auth': {
+        target: 'https://firebase.firebaseapp.com',
+        changeOrigin: true
+      }
+    }
+  }
 })
