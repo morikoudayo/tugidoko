805290fa447a6b5e5d90e7906fb3a0ef1bc2f319
Koutarou Mori <m.koutarou2004@gmail.com>
2025-09-28 07:33:52 +0000

    change variable name

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
-const LOCAL_DOMAIN = process.env.VITE_DOMAIN || 'localhost'
+const DOMAIN = process.env.VITE_DOMAIN || 'localhost'
 
 const createProxyConfig = (target: string, host: string) => ({
   target,
   changeOrigin: true,
-  cookieDomainRewrite: LOCAL_DOMAIN,
+  cookieDomainRewrite: DOMAIN,
   headers: { Host: host },
   followRedirects: false,
   configure: (proxy: any) => {
@@ -30,7 +30,7 @@ export default defineConfig({
   server: {
     https: process.env.VITE_HTTPS === 'true' ? {} : undefined,
     host: '0.0.0.0',
-    allowedHosts: [LOCAL_DOMAIN],
+    allowedHosts: [DOMAIN],
     port: 3000,
     proxy: {
       '/amserver': createProxyConfig('https://mastersso.kanagawa-u.ac.jp', 'mastersso.kanagawa-u.ac.jp'),
