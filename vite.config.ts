417d21510d7aa4da8f361ef49c08dc6b2fc07be6
Koutarou Mori <m.koutarou2004@gmail.com>
2025-09-30 14:03:08 +0900

    settings ssl properly

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const DOMAIN = process.env.VITE_DOMAIN || 'localhost'
 
@@ -25,19 +26,26 @@ const createProxyConfig = (target: string, host: string) => ({
   }
 })
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    https: process.env.VITE_HTTPS === 'true' ? {} : undefined,
-    host: '0.0.0.0',
-    allowedHosts: [DOMAIN],
-    port: 3000,
-    proxy: {
-      '/amserver': createProxyConfig('https://mastersso.kanagawa-u.ac.jp', 'mastersso.kanagawa-u.ac.jp'),
-      '/campusweb': createProxyConfig('https://webstation.kanagawa-u.ac.jp', 'webstation.kanagawa-u.ac.jp'),
-      '/__/auth': {
-        target: 'https://firebase.firebaseapp.com',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  const DOMAIN = env.VITE_DOMAIN || 'localhost'
+  const HTTPS = env.VITE_HTTPS === 'true'
+
+  return {
+    plugins: [react(), tsconfigPaths(), HTTPS ? basicSsl() : undefined],
+    server: {
+      https: HTTPS ? {} : undefined,
+      host: '0.0.0.0',
+      allowedHosts: [DOMAIN],
+      port: 3000,
+      proxy: {
+        '/amserver': createProxyConfig('https://mastersso.kanagawa-u.ac.jp', 'mastersso.kanagawa-u.ac.jp'),
+        '/campusweb': createProxyConfig('https://webstation.kanagawa-u.ac.jp', 'webstation.kanagawa-u.ac.jp'),
+        '/__/auth': {
+          target: 'https://firebase.firebaseapp.com',
+          changeOrigin: true
+        }
       }
     }
   }
