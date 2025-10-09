379f790358d65f82f562acb50fc9592fac1601db
Koutarou Mori <m.koutarou2004@gmail.com>
2025-10-09 16:45:49 +0900

    getAbsenceのデータ取得とパース処理を分離し、テストを追加
    参照するテーブルを正常化
    
    absence.htmlを用いてテストを簡略化

--- src/components/AfterLogin/getAbsenceCount.ts
+++ src/components/AfterLogin/getAbsenceCount.ts
@@ -3,19 +3,10 @@ import { parseHTML } from "./getSchedule";
 export type AbsenceCounts = Map<string, number>;
 
 /**
- * 授業の欠席回数を学内ポータルから取得しschedule内の各ClassDataのabsenceCountに代入する。
+ * HTMLドキュメントから欠席回数情報をパースする
  */
-export async function getAbsenceCounts(): Promise<AbsenceCounts> {
-  const response = await fetch('/campusweb/campussquare.do?_flowId=AAW0001000-flow&link=menu-link-mf-135117')
-
-  const html = await response.text()
-
-  const document = parseHTML(html)
-
-  const currentMonth = new Date().getMonth();
-  const index = 3 <= currentMonth && currentMonth <= 8 ? -1 : -2;
-
-  const attendancesTable = Array.from(document.querySelectorAll('tbody')).at(index);
+export function parseAbsenceCounts(document: Document): AbsenceCounts {
+  const attendancesTable = Array.from(document.querySelectorAll('tbody')).at(-2);
 
   const absenceCounts = new Map<string, number>();
   Array.from(attendancesTable!.rows).forEach((row) => {
@@ -24,11 +15,24 @@ export async function getAbsenceCounts(): Promise<AbsenceCounts> {
     const className = classNameElement.textContent.trim();
 
     const match = row.innerHTML.match(/\b\d{1,2}\/\d{1,2}\/\d{1,2}\/\d{1,2}\b/);
-    const attendance = match![0].split('/')
+    if (!match) return;
+    
+    const attendance = match[0].split('/')
     const absenceCount = Number(attendance[2]) * 0.5 + Number(attendance[3]) //欠席を1、遅刻を0.5として欠席数を計算。
 
     absenceCounts.set(className, absenceCount)
   })
 
   return absenceCounts
+}
+
+/**
+ * 授業の欠席回数を学内ポータルから取得しschedule内の各ClassDataのabsenceCountに代入する。
+ */
+export async function getAbsenceCounts(): Promise<AbsenceCounts> {
+  const response = await fetch('/campusweb/campussquare.do?_flowId=AAW0001000-flow&link=menu-link-mf-135117')
+  const html = await response.text()
+  const document = parseHTML(html)
+  
+  return parseAbsenceCounts(document);
 }
